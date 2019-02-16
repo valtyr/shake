5618da45d38992426c009ac694d61ce3581a90bc
Valtýr Örn Kjartansson <valtyr@gmail.com>
2019-02-16 19:38:00 +0000

    Add error tests

--- test/event_test.go
+++ test/event_test.go
@@ -9,7 +9,20 @@ import (
 
 func TestEventParsing(t *testing.T) {
 	rawJSON := []byte(`{"type": "app_opened", "fields": {"active_screen": "home"}}`)
-	result, _ := event.DecodeJSON(rawJSON)
+	result, err := event.DecodeJSON(rawJSON)
+	assert.NoError(t, err)
 	assert.Equal(t, result.Type, "app_opened")
 	assert.Equal(t, result.Fields["active_screen"].(string), "home")
+}
+
+func TestEventParsingNoTypeError(t *testing.T) {
+	rawJSON := []byte(`{"fields": {"active_screen": "home"}}`)
+	_, err := event.DecodeJSON(rawJSON)
+	assert.Error(t, err)
+}
+
+func TestEventParsingFieldsError(t *testing.T) {
+	rawJSON := []byte(`{"type": "app_opened", "fields": 4}`)
+	_, err := event.DecodeJSON(rawJSON)
+	assert.Error(t, err)
 }
